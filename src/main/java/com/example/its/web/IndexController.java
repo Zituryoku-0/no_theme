b93ab2718be32fbff2290521da40e890e439b9c4
zituryoku0 <tommykouki1031@icloud.com>
2023-01-09 16:19:15 +0900

    Initial commit

---
+++ src/main/java/com/example/its/web/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.its.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    // GET /
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
